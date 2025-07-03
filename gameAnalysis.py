171d311cc880ef14d76bf97171f9a355d8972089
Kurumi <faizsahmed@gmail.com>
2025-07-03 15:51:39 +0500

    prevents user from entering invalid data (specifically files that don't exist in the "filesForAnalysis" directory)

--- gameAnalysis.py
+++ gameAnalysis.py
@@ -1,6 +1,5 @@
 import os, json, datetime, markdown, time, threading, sys
 import pathlib
-
 from google import genai
 from google.genai import types
 
@@ -16,7 +15,7 @@ if not os.path.exists("savedData.json"):
             "Default": "These are documents containing level data (filesDescription). gameDescription Among the levels without survivorship bias, which levels do players like and dislike? Among the levels with survivorship bias, which levels do players like and dislike? Make your own algorithm to decide when survivorship bias occurs based on when the spread of player drop-off rates stabilizes. Compared to other levels, why are these levels liked or disliked? Based on the correlations between various data points, are there any insights on player behavior based on the contents of each level and features introduced in the progression map? After this, provide a concise response about actionable insights to the gameâ€™s developers. Separate sections for players with/without survivorship bias with subsections for what the game developers should add/remove/improve/rework. Throughout your response, make connections to the features of similar games through research."
         },
         "Files Descriptions": {
-            "Default": '''"RAW Game Data Sheets - Level Data.csv": contents of each level, "RAW Game Data Sheets - Progression Map.csv": progression map across levels, "RAW Game Data Sheets - Raw Analysis Data.csv": raw player data for each level'''
+            "Default": '''"RAW Game Data Sheets - Level Data.csv": contents of each level || "RAW Game Data Sheets - Progression Map.csv": progression map across levels || "RAW Game Data Sheets - Raw Analysis Data.csv": raw player data for each level'''
         },
         "Game Descriptions": {
             "Word Nut": "This is for a casual mobile game in which players must sort jumbled characters to form words in a crossword puzzle."
@@ -27,7 +26,7 @@ if not os.path.exists("savedData.json"):
 
 # load from 'savedData.json' for modification/extraction of entries
 tempFile = open("savedData.json", 'r')
-savedData = json.load(tempFile)
+savedData: dict = json.load(tempFile)
 tempFile.close()
 
 
@@ -35,30 +34,51 @@ tempFile.close()
 def entryProcess(entries: dict):
     print('')
     selectedEntryName = ""
-    while selectedEntryName not in entries.keys():
+    invalidEntries = {'': "Input cannot be empty."}  # format is {entry: reason}
+    if userEntry == "Files Descriptions":
+        # don't allow the selection of file descriptions for files that don't exist
+        for fd in savedData[userEntry]:
+            missingFileDesc = ""
+            for f in savedData[userEntry][fd].split(" || "):
+                f = f.split(": ")[0][1:-1]
+                if f not in os.listdir("filesForAnalysis"):
+                    missingFileDesc += f'"{f}", '
+                missingFileDesc = missingFileDesc[:-1]
+            if missingFileDesc != "":
+                invalidEntries.update({fd: f"Missing file(s): {missingFileDesc}"})
+
+    while selectedEntryName not in entries:
         for entry in entries:
-            print(f"{entry}: {entries[entry]}")
+            if entry not in invalidEntries:
+                print(f"{entry}: {entries[entry]}")
+            else:
+                print(f"{entry}: INVALID ({invalidEntries[entry]})")
         selectedEntryName = input("Select an entry above or make a new entry: ")
         if (selectedEntryName.split(' ')[0].lower() == "remove") and (selectedEntryName[7:] in entries.keys()):
             print('')
             entries.pop(selectedEntryName[7:])
-        elif selectedEntryName == '':
+        elif selectedEntryName in invalidEntries:
+            print(invalidEntries[selectedEntryName])
+            selectedEntryName = ''
             print('')
             continue
-        elif selectedEntryName not in entries.keys():
+        elif selectedEntryName not in entries:
             userInput = ''
             while (userInput != 'y') and (userInput != 'n'):
                 userInput = input("Create new entry? 'y' / 'n': ").lower()
             if userInput == 'y':
-                if list(savedData.keys())[userSelection - 1] == "Files Descriptions":
+                if userEntry == "Files Descriptions":
                     filesDesc = ""
-                    for ffa in os.scandir("filesForAnalysis"):
-                        ffaDesc = ""
-                        while ffaDesc == "":
-                            ffaDesc = input(f'Enter a brief description for the file "{ffa.name}": ')
-                        filesDesc += f'"{ffa.name}": {ffaDesc}, '
-                    filesDesc = filesDesc[:-2]
-                    entries.update({selectedEntryName: filesDesc})
+                    if os.listdir("filesForAnalysis"):
+                        for ffa in os.listdir("filesForAnalysis"):
+                            ffaDesc = ""
+                            while ffaDesc == "":
+                                ffaDesc = input(f'Enter a brief description for the file "{ffa}": ')
+                            filesDesc += f'"{ffa}": {ffaDesc} || '
+                        filesDesc = filesDesc[:-4]
+                        entries.update({selectedEntryName: filesDesc})
+                    else:
+                        raise FileNotFoundError("filesForAnalysis cannot be empty!")
                 else:
                     entries.update({selectedEntryName: input(f'Enter contents of "{selectedEntryName}": ')})
                     print('')
@@ -88,23 +108,25 @@ selectedEntryNames = []
 for k in savedData:
     selectedData.update({k: ""})
     selectedEntryNames.append("")
-userSelection = -1
-while (userSelection != 0) or any(e == "" for e in selectedData.values()):
+userIdx = -1
+while (userIdx != 0) or any(e == "" for e in selectedData.values()):
     print('')
     for i, k in enumerate(savedData.keys()):
         print(f"{i + 1}: {k} -- {selectedEntryNames[i] if selectedData[k] else "*NOT CHOSEN*"}")
     try:
-        userSelection = int(input('Select an entry above or enter "0" to query the AI: '))
+        userIdx = int(input('Select an entry above or enter "0" to query the AI: '))
     except ValueError:
         continue
-    if (userSelection > 0) and (userSelection <= len(savedData)):
-        selectedEntryNames[userSelection - 1], savedData[list(savedData.keys())[userSelection - 1]] = entryProcess(savedData[list(savedData.keys())[userSelection - 1]])
-        selectedData[list(savedData.keys())[userSelection - 1]] = savedData[list(savedData.keys())[userSelection - 1]][selectedEntryNames[userSelection - 1]]
+    if (userIdx > 0) and (userIdx <= len(savedData)):
+        userEntry = list(savedData.keys())[userIdx - 1]
+        selectedEntryNames[userIdx - 1], savedData[userEntry] = entryProcess(savedData[userEntry])
+        selectedData[userEntry] = savedData[userEntry][selectedEntryNames[userIdx - 1]]
 tempFile = open("savedData.json", 'w')
 json.dump(savedData, tempFile, indent=2)
 tempFile.close()
 
 prompt = selectedData["Prompts"].replace("filesDescription", selectedData["Files Descriptions"]).replace("gameDescription", selectedData["Game Descriptions"])
+
 
 def queryGemini():
     gemini = genai.Client(api_key=APIkeys["gemini"])
@@ -116,6 +138,7 @@ def queryGemini():
         config=types.GenerateContentConfig(thinking_config=types.ThinkingConfig(include_thoughts=True))  # include thinking in response
     )
 
+
 def stopwatch():
     print('')
     timeElapsed: float = 0
@@ -124,6 +147,7 @@ def stopwatch():
         sys.stdout.flush()
         time.sleep(0.25)
         timeElapsed += 0.25
+
 
 geminiQuerier = threading.Thread(target=queryGemini)
 stopwatchThread = threading.Thread(target=stopwatch)
